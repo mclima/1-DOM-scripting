86fb0136a04da32ada8312d0cc38dad006c5dd85
mclima <mcamil2009@gmail.com>
2024-03-19 21:45:24 -0400

    [preview] final commit for this lesson

--- app/js/index.js
+++ app/js/index.js
@@ -1,16 +1,3 @@
-const nav = document.querySelector('.main-menu');
-const navList = document.createElement('ul');
+import { makeNav } from './modules/nav.js';
 
-// navItemsArray.forEach(item => {
-//     let listItem = document.createElement('li');
-//     listItem.innerHTML = `<a href="#">${item}</a>`;
-//     navList.appendChild(listItem);
-// });
-
-navItemsObject.forEach(function (item) {
-    let listItem = document.createElement('li');
-    listItem.innerHTML = `<a href="${item.link}">${item.label}</a>`;
-    navList.appendChild(listItem);
-});
-
-nav.append(navList);
+makeNav();
